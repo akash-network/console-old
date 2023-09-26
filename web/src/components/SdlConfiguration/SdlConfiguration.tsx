ae906474d15ca649fbcacf51a8144cae416e7a3a
Joseph Tary <joseph.tary@gmail.com>
2023-09-26 16:30:29 -0700

    fix: hide gpu

--- web/src/components/SdlConfiguration/SdlConfiguration.tsx
+++ web/src/components/SdlConfiguration/SdlConfiguration.tsx
@@ -56,7 +56,7 @@ export const SdlConfiguration: React.FC<SdlConfigurationProps> = ({
   const [getRpcNode] = useRpcNode();
   const forbidEditing = configurationType === SdlConfigurationType.Update;
   const hasGPU = getRpcNode().networkType === 'testnet';
-  const showGPU = true;
+  const showGPU = false;
 
   return (
     <React.Fragment>
